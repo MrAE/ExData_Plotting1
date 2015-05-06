00c9b334bf225fe0c2e6c931d7828e6edda26c91
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2015-05-05 22:58:36 -0400

    Plot 3 done.

---
+++ plot3.R
@@ -0,0 +1,38 @@
+###
+### Coursera: Exploratory Data Analysis
+###
+### Jesse Leigh Patsolic 
+### 2015 <studiojlp@gmail.com>
+### S.D.G 
+#
+#! /usr/bin/R
+
+hpc <- read.csv("household_power_consumption.txt", sep=";", na.strings="?", skip=66600, nrows=4000)
+
+cn <- colnames(read.csv("household_power_consumption.txt", sep=";", nrows=2))
+
+colnames(hpc) <- cn
+
+hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
+hpc$Time <- strptime(paste(hpc$Date,hpc$Time, sep=" "), 
+                     format="%Y-%m-%d %H:%M:%S")
+
+sub <- hpc[hpc$Date <= as.Date("2007-02-02") & 
+           hpc$Date >= as.Date("2007-02-01"),]
+
+
+
+png("plot3.png", height=480, width=480)
+plot(sub$Time, sub$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(sub$Time, sub$Sub_metering_2, col="red")
+lines(sub$Time, sub$Sub_metering_3, col="blue")
+legend("topright", legend=grep("Sub",colnames(sub),value=T), col=c(1,2,"blue"), lty=1)
+dev.off()
+
+
+
+
+#   Time: 30 minutes
+##  Working status: Check. Creates the graph as desired.
+### Comments:
+####Soli Deo Gloria
